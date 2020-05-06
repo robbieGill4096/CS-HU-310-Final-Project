9a8f0d8a0c27ada9441e276555e277227c89159d
Robbie Gill <34260042+robbieGill4096@users.noreply.github.com>
2020-05-06 00:42:48 -0600

    Update sqlcode.sql

--- sqlcode.sql
+++ sqlcode.sql
@@ -3,31 +3,27 @@ USE FinalProject;
 
 
 CREATE TABLE Item
-(
-ID int auto_increment,
-ItemCode varchar(10) NOT NULL,
-UNIQUE (ItemCode),
+(ID int auto_increment,
+ItemCode varchar(10) UNIQUE,
 ItemDescription varchar(50),
 Price decimal(4,2) DEFAULT 0,
-primary key(id)
-);
+primary key(ID));
 
 CREATE TABLE Purchase
-(
-ID int auto_increment,
+(ID int auto_increment,
 ItemID int NOT NULL,
 Quantity int NOT NULL,
 PurchaseDate datetime DEFAULT CURRENT_TIMESTAMP,
-primary key(id),
-FOREIGN KEY (ID) REFERENCES Item(ID)
-);
+primary key(ID));
+          
+ADD FOREIGN KEY (ItemID) REFERENCES Item(ID);
 
 CREATE TABLE Shipment
-(
-ID int auto_increment,
+(ID int auto_increment,
 ItemID int NOT NULL,
 Quantity int NOT NULL,
 ShipmentDate date NOT NULL UNIQUE,
-primary key(id),
-FOREIGN KEY (ID) REFERENCES Item(ID)
-);
+primary key(ID));
+            
+Alter table Shipment
+ADD FOREIGN KEY (ItemID) REFERENCES Item(ID);
